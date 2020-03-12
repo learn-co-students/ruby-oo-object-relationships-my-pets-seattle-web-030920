03de989356664bc9f5b399b8e4241eaaebdc267f
github username <github email address>
2020-03-12 12:55:42 -0700

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
   # code goes here
+  attr_reader :name 
+  attr_accessor :owner, :mood
+  @@all=[]
+  def initialize(name, owner)
+    @name=name
+    @species= "cat"
+    @owner=owner
+    @@all << self
+    @mood="nervous"
+  end
+def self.all
+  @@all
+end
+
 end
